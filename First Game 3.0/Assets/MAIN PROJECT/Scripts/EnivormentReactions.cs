64101b55e42dd360b88c28c2591c10639474486d
Nathan Fishel <n_fishel-v@dcu.org>
2021-03-24 13:10:09 -0400

    game so far

---
+++ First Game 3.0/Assets/MAIN PROJECT/Scripts/EnivormentReactions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnivormentReactions : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Coins"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
