3d9caea28180ad0d994ecbb890d4dc26e265da83
Nick <nick.gilliam2@gmail.com>
2021-03-29 14:05:02 -0400

    added health bar and pickups

---
+++ First Game 3.0/Assets/HealthHeartSystem/Scripts/PlayerStats.cs
@@ -0,0 +1,54 @@
+﻿/*
+ *  Author: ariel oliveira [o.arielg@gmail.com]
+ */
+
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+    [SerializeField]
+    private float health, maxHealth, maxTotalHealth;
+
+    public delegate void OnHealthChangedDelegate();
+    public OnHealthChangedDelegate onHealthChangedCallback;
+
+    private static PlayerStats instance;
+
+    public void Heal(float health) {
+        this.health += health;
+        ClampHealth();
+    }
+
+    public void TakeDamage(float dmg) {
+        health -= dmg;
+        ClampHealth();
+    }
+
+    public void AddHealth() {
+        if (maxHealth < maxTotalHealth) {
+            maxHealth += 1;
+            health = maxHealth;
+
+            if (onHealthChangedCallback != null)
+                onHealthChangedCallback.Invoke();
+        }
+    }
+
+    void ClampHealth() {
+        health = Mathf.Clamp(health, 0, maxHealth);
+
+        if (onHealthChangedCallback != null)
+            onHealthChangedCallback.Invoke();
+    }
+
+    public float Health { get { return health; } }
+    public float MaxHealth { get { return maxHealth; } }
+    public float MaxTotalHealth { get { return maxTotalHealth; } }
+
+    public static PlayerStats Instance {
+        get {
+            if (instance == null)
+                instance = FindObjectOfType<PlayerStats>();
+            return instance;
+        }
+    }
+}
